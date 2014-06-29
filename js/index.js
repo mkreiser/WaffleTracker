4a9fdb6e1af7bfadbd2e69bd88f65f2d94645033
Mike Kreiser <mkreise2@illinois.edu>
2014-06-29 18:08:39 -0400

    alpha 1e

--- js/index.js
+++ js/index.js
@@ -12,78 +12,7 @@ if (bitcoinAddress == ""){bitcoinAddress = "";}
 $('#waffleForm').val(waffleAddress);
 $('#bitForm').val(bitcoinAddress);
 
-var url =  "http://wafflepool.com/api/miner?address=";
-url += waffleAddress;
-url = encodeURIComponent(url);
-url = 'http://jsonp.guffa.com/Proxy.ashx?url=' + url;
-
-$.ajax({
-    url: url,
-    dataType: 'jsonp',
-    success: function(data) {
-
-        var url2 = 'coinbase.com/api/v1/currencies/exchange_rates';
-        url2 = encodeURIComponent(url2);
-        url2 = 'http://jsonp.guffa.com/Proxy.ashx?url=' + url2;
-    
-        $.ajax({
-        url: url2,
-        dataType: 'jsonp',
-        success: function(results){
-
-            $('#scryptHash').html(data.scrypt.hashrate_str);
-            $('#scryptnHash').html(data.nscrypt.hashrate_str);
-            $('#x11Hash').html(data.x11.hashrate_str);
-
-            var sTotal = data.scrypt.balances.sent + data.scrypt.balances.confirmed + data.scrypt.balances.unconverted;
-            var snTotal = data.nscrypt.balances.sent + data.nscrypt.balances.confirmed + data.nscrypt.balances.unconverted;
-            var xTotal = data.x11.balances.sent + data.x11.balances.confirmed + data.x11.balances.unconverted;
-
-            $('#scryptSent').html(roundToEight(data.scrypt.balances.sent) + " BTC");
-            $('#scryptUnsent').html(roundToEight(data.scrypt.balances.confirmed + data.scrypt.balances.unconverted) + " BTC");
-            $('#scryptTotal').html(roundToEight(sTotal) + " BTC");
-
-            $('#scryptnSent').html(roundToEight(data.nscrypt.balances.sent) + " BTC");
-            $('#scryptnUnsent').html(roundToEight(data.nscrypt.balances.confirmed + data.nscrypt.balances.unconverted) + " BTC");
-            $('#scryptnTotal').html(roundToEight(snTotal) + " BTC");
-
-            $('#x11Sent').html(roundToEight(data.x11.balances.sent) + " BTC");
-            $('#x11Unsent').html(roundToEight(data.x11.balances.confirmed + data.x11.balances.unconverted) + " BTC");
-            $('#x11Total').html(roundToEight(xTotal) + " BTC");
-
-            $('#totalEarned').html(roundToEight(sTotal + snTotal + xTotal) + " BTC");
-            $('#totalSent').html(roundToEight(data.scrypt.balances.sent + data.nscrypt.balances.sent + data.x11.balances.sent) + " BTC");
-        }
-    });
-    }
-
-});
-
-var url3 = "http://blockchain.info/address/";
-url3 += bitcoinAddress;
-url3 += "?format=json";
-url3 = encodeURIComponent(url3);
-url3 = "http://jsonp.guffa.com/Proxy.ashx?url=" + url3;
-
-$.ajax({
-    url: url3,
-    dataType: 'jsonp',
-    success: function(data) {
-        var url2 = 'coinbase.com/api/v1/currencies/exchange_rates';
-        url2 = encodeURIComponent(url2);
-        url2 = 'http://jsonp.guffa.com/Proxy.ashx?url=' + url2;
-    
-        $.ajax({
-        url: url2,
-        dataType: 'jsonp',
-        success: function(results){
-            $('#bitAm').html(data.final_balance/100000000 + " BTC");
-            $('#usdAm').html(roundToTwo(results.btc_to_usd * data.final_balance/100000000) + " USD")
-        }
-    });
-
-    }
-});
+update();
 
 $('#updateButton').click(function(){
         waffleAddress = $('#waffleForm').val();
